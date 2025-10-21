67ec50e52ca5cd5366d71a21540e6c83efa81ea7
Rodney Swaji <rodney.swaji@student.moringaschool.com>
2025-10-21 14:53:15 +0300

    initial commit

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,15 @@
 import React from "react";
+import Links from "./Link";
 
-function About() {
+function About({ bio, github, linkedin }) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
-      <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      {bio && <p>{bio}</p>}
+      <Links github={github} linkedin={linkedin} />
     </div>
   );
 }
 
 export default About;
+
